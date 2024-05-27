a9973e5575dfb43b9aaaad7bd173aac73c3ae19d
Mingchen Ju <traveller31415@gmail.com>
2024-05-27 06:33:07 +0000

    Make some modifacation on watchtower

--- autoDeploy/updateNodeProj.sh
+++ autoDeploy/updateNodeProj.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-cd SeconFun
+REPO_LINK="git@github.com:Maka314/SeconFun.git"
+FOLDER_NAME="SeconFun"
+
+cd "$FOLDER_NAME" || { echo "Failed to change directory to $FOLDER_NAME"; exit 1; }
 
 current_branch=$(git rev-parse --abbrev-ref HEAD)
 
@@ -9,8 +12,12 @@ remote_commit=$(git ls-remote origin -h "refs/heads/$current_branch" | cut -f1)
 
 if [ "$local_commit" != "$remote_commit" ]; then
     echo "There is a update"
+    screen -X -S "$FOLDER_NAME" quit
+    screen -r "$FOLDER_NAME"
     git pull
+    npm install
+    npm run build
+    npm start
 else
     echo "No need to update"
 fi
-
